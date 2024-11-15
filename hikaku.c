6baf15017c83e1c5ed14d7fc049c580878ce9099
じょん・すみす <takurou70@icloud.com>
2024-11-15 18:19:13 +0900

    Add files via upload

--- hikaku.c
+++ hikaku.c
@@ -3,21 +3,21 @@
 int main(void)
 {
     int a, b, c;
-    printf("２つの数字を入力してください：");
+    printf("定数a,bをそれぞれ入力してください：");
     scanf("%d %d", &a, &b);
     if(a > b)
     {
         c = a - b;
-        printf("%dのほうが%dより%d大きい.\n", a, b, c);
+        printf("a=%dのほうがb=%dより%d大きい.\n", a, b, c);
     }
     else if(a < b)
     {
         c = b - a;
-        printf("%dのほうが%dより%d大きい.\n", b, a, c);
+        printf("b=%dのほうがa=%dより%d大きい.\n", b, a, c);
     }
     else
     {
-        printf("%dと%dは等しい.\n", a, b);
+        printf("二つの数字a,bは等しい.\n", a, b);
     }
     return 0;
 }
